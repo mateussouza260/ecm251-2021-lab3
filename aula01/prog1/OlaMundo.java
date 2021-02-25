f5fe37ef51edf7039caf3dd1de889ced46b8f710
mateussouza <70783690+matias260@users.noreply.github.com>
2021-02-25 09:21:37 -0300

    Alteração na base

--- aula01/prog1/OlaMundo.java
+++ aula01/prog1/OlaMundo.java
@@ -1,7 +1,7 @@
-package aula01.prog1;
-
 class OlaMundo {
     public static void main(String[] args) {
-        System.out.println("Olá mundo");
+        System.out.println("Ola mundo");
+        System.out.println("Stein é mal?");
+        System.out.println("Overwatch morreu?");
     }
 }
